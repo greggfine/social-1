1dd947e7af1777a80fe8410d5027b4263a17b171
Gregg Fine <bbeez8500@gmail.com>
2020-04-27 15:08:20 -0400

    uncommented setupProxj.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,8 @@
-// const proxy = require("http-proxy-middleware")
-// module.exports = function(app) {
-//   app.use(
-//     proxy(["/api/tracks/**", "/api/comments/**", "/audio/**", "/imgs/**", "/api/members/**"], {
-//       target: "http://localhost:3001/"
-//     })
-//   )
-// }
+const proxy = require("http-proxy-middleware")
+module.exports = function(app) {
+  app.use(
+    proxy(["/api/tracks/**", "/api/comments/**", "/audio/**", "/imgs/**", "/api/members/**"], {
+      target: "http://localhost:3001/"
+    })
+  )
+}
