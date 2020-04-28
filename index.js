7269bc9f77d64088dca0fbf88d44e4b055ff36db
Gregg Fine <bbeez8500@gmail.com>
2020-04-28 11:35:05 -0400

    commented out http-proxy-middleware

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
 app.use(helmet());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.json());
