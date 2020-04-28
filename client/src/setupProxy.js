026a222291455aac7c7e14f5c6f7d93a4945ec12
Gregg Fine <bbeez8500@gmail.com>
2020-04-28 10:44:46 -0400

    commented out setupProxy for experiment

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,8 @@
-const proxy = require("http-proxy-middleware")
-module.exports = function(app) {
-  app.use(
-    proxy(["/api/tracks/**", "/api/comments/**", "/audio/**", "/imgs/**", "/api/members/**"], {
-      //   target: "http://localhost:3001/"
-      target: "https://salty-bayou-12671.herokuapp.com/"
-    })
-  )
-}
+// const proxy = require("http-proxy-middleware")
+// module.exports = function(app) {
+//   app.use(
+//     proxy(["/api/tracks/**", "/api/comments/**", "/audio/**", "/imgs/**", "/api/members/**"], {
+//       target: "http://localhost:3001/"
+//     })
+//   )
+// }
