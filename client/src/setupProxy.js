fffe5d9123bc36e877e39840bf7d6100256cdc10
Gregg Fine <bbeez8500@gmail.com>
2020-04-28 10:39:44 -0400

    changed setupProxy target for experiment

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,8 @@ const proxy = require("http-proxy-middleware")
 module.exports = function(app) {
   app.use(
     proxy(["/api/tracks/**", "/api/comments/**", "/audio/**", "/imgs/**", "/api/members/**"], {
-      target: "http://localhost:3001/"
+      //   target: "http://localhost:3001/"
+      target: "https://salty-bayou-12671.herokuapp.com/"
     })
   )
 }
