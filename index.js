ecfd7203923c2dec72d215e1107d770b16fdc128
Gregg Fine <bbeez8500@gmail.com>
2020-04-29 11:13:32 -0400

    111

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ io.on("connect", socket => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
